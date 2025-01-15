6bc45ab38e8e61c79b016f8f996135fee9626e38
Faizah <faizah087@hotmail.co.uk>
2025-01-15 20:19:28 +0000

    change 1

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 #needed to install pandas and matplotlib + openpyxl
 #replit only allowed two members (one contributor)
 #github issue - cloning project to pycharm and pushing project to github so we could all contribute
-#hi
+#hihi
 
 import pandas as pd
 import matplotlib.pyplot as plt
